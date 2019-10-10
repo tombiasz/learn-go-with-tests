e3553bd7a209e6229b4b9fd0eb3bfaf64062d78a
tombiasz <tomasz.m.nowacki@gmail.com>
2019-10-10 21:21:06 +0200

    initial commit

---
+++ 01_hello/hello_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Error("got $q want $q", got, want)
+		}
+	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
