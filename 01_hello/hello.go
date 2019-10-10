6980005401e673ac3713de0d345571f2b9c61f31
tombiasz <tomasz.m.nowacki@gmail.com>
2019-10-10 21:58:40 +0200

    refactor: extract function

--- 01_hello/hello.go
+++ 01_hello/hello.go
@@ -13,17 +13,22 @@ func Hello(name string, language string) string {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingsPrefix(language) + name
+}
 
+func greetingsPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 
 	case french:
 		prefix = frenchHelloPrefix
+
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }
 
 func main() {
