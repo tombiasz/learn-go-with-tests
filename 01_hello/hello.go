9f5965311893fa66dc5fb299c966e4dee60dbc18
tombiasz <tomasz.m.nowacki@gmail.com>
2019-10-10 21:45:43 +0200

    handle spanish language

--- 01_hello/hello.go
+++ 01_hello/hello.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "hello, "
+const spanishHelloPrefix = "hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- 01_hello/hello_test.go
+++ 01_hello/hello_test.go
@@ -13,14 +13,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
