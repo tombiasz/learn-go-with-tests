13049a61c386dac022e6267edcf9ce786059841c
tombiasz <tomasz.m.nowacki@gmail.com>
2019-11-28 21:55:44 +0100

    add dictionary implementation

---
+++ 07_dictionary/dictionary.go
@@ -0,0 +1,59 @@
+package main
+
+const (
+	ErrNotFound     = DictionaryErr("could not found the error you are looking for")
+	ErrWordExists   = DictionaryErr("cannot add word because it already exists")
+	ErrWordNotExist = DictionaryErr("could not update because word does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
